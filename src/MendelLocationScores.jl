e4990249b247cb4634814b234ac70cf7580871e5
Eric Sobel <esobel@ucla.edu>
2016-10-06 12:38:01 -0700

    Organized likelihood coding so that the analysis-specific prior, penetrance, and transmission functions are local to the analysis option and then the function names are passed to the likelihood evaluation program.

--- src/MendelLocationScores.jl
+++ src/MendelLocationScores.jl
@@ -39,7 +39,7 @@ function LocationScores(control_file = ""; args...)
   # The user specifies the analysis to perform via a set of keywords.
   # Start the keywords at their default values.
   #
-  keyword = set_keyword_defaults!(Dict{ASCIIString, Any}())
+  keyword = set_keyword_defaults!(Dict{AbstractString, Any}())
   #
   # Keywords unique to this analysis should be first defined here
   # by setting their default values using the format:
@@ -99,9 +99,9 @@ the flanking markers occur in balanced numbers to the left and
 right of the trait locus.
 """
 function location_scores_option(pedigree::Pedigree, person::Person,
-  nuclear_family::NuclearFamily, locus::Locus, locus_frame::DataFrame, 
+  nuclear_family::NuclearFamily, locus::Locus, locus_frame::DataFrame,
   phenotype_frame::DataFrame, pedigree_frame::DataFrame,
-  keyword::Dict{ASCIIString, Any})
+  keyword::Dict{AbstractString, Any})
   #
   # Problem formulation checks.
   #
@@ -168,8 +168,8 @@ function location_scores_option(pedigree::Pedigree, person::Person,
   #
   # Define an empty dataframe to hold the mapping results.
   #
-  lodscore_frame = DataFrame(LeftFlank = ASCIIString[],
-    Trait = ASCIIString[], RightFlank = ASCIIString[],
+  lodscore_frame = DataFrame(LeftFlank = AbstractString[],
+    Trait = AbstractString[], RightFlank = AbstractString[],
     XXCentiMorgans = Float64[], XYCentiMorgans = Float64[],
     LodScore = Float64[])
   #
@@ -192,12 +192,13 @@ function location_scores_option(pedigree::Pedigree, person::Person,
     locus.model_locus[1] = trait_place
     locus.morgans[:, trait_place] = - Inf
     (saved_locus_trait, locus.trait) = (locus.trait, 1)
-    keyword["analysis_option"] = ""
+    keyword["analysis_option"] = "StandardizingLoglikelihood"
     #
     # Define the parameter data structure.
     #
     parameter = set_parameter_defaults(keyword)
-    parameter = initialize_optimization(locus, parameter, keyword)
+    parameter =
+      initialize_optimization_location_score!(locus, parameter, keyword)
     #
     # Calculate recombination fractions between adjacent model loci.
     #
@@ -205,7 +206,7 @@ function location_scores_option(pedigree::Pedigree, person::Person,
     #
     # Fetch the instructions for conducting the Elston-Stewart algorithm.
     #
-    (instruction, elston_stewart_count) = orchestrate_likelihood(pedigree, 
+    (instruction, elston_stewart_count) = orchestrate_likelihood(pedigree,
       person, nuclear_family, locus, keyword)
     if any(elston_stewart_count .>  keyword["complexity_threshold"])
       println(io, "Marker $locus.name[loc] was skipped because one or more ",
@@ -218,8 +219,10 @@ function location_scores_option(pedigree::Pedigree, person::Person,
     #
     # Evaluate the standardizing loglikelihood.
     #
-    loglikelihood_at_infinity = elston_stewart_loglikelihood(pedigree, person,
-      locus, parameter, instruction, keyword)
+    loglikelihood_at_infinity =
+      elston_stewart_loglikelihood(penetrance_location_score,
+      prior_location_score, transmission_location_score,
+      pedigree, person, locus, parameter, instruction, keyword)
     #
     # Restore the model loci to their proper order.
     #
@@ -252,7 +255,8 @@ function location_scores_option(pedigree::Pedigree, person::Person,
     # Define the parameter data structure.
     #
     parameter = set_parameter_defaults(keyword)
-    parameter = initialize_optimization(locus, parameter, keyword)
+    parameter =
+      initialize_optimization_location_score!(locus, parameter, keyword)
     #
     # Calculate recombination fractions between adjacent model loci.
     #
@@ -260,7 +264,7 @@ function location_scores_option(pedigree::Pedigree, person::Person,
     #
     # Fetch the instructions for conducting the Elston-Stewart algorithm.
     #
-    (instruction, elston_stewart_count) = orchestrate_likelihood(pedigree, 
+    (instruction, elston_stewart_count) = orchestrate_likelihood(pedigree,
       person, nuclear_family, locus, keyword)
     if any(elston_stewart_count .>  keyword["complexity_threshold"])
       println(io, "Marker $locus.name[loc] was skipped because one or more ",
@@ -274,8 +278,9 @@ function location_scores_option(pedigree::Pedigree, person::Person,
     #
     function fun(par)
       copy!(parameter.par, par)
-      f = elston_stewart_loglikelihood(pedigree, person, locus, parameter, 
-        instruction, keyword)
+      f = elston_stewart_loglikelihood(penetrance_location_score,
+        prior_location_score, transmission_location_score,
+        pedigree, person, locus, parameter, instruction, keyword)
       return (f, nothing, nothing)
     end # function fun
     (best_par, best_value) = optimize(fun, parameter)
@@ -341,5 +346,206 @@ function update_location_score_markers(list::Vector{Int}, i::Int, l::Int,
   return (i, list)
 end # function update_location_score_markers
 
-end # module MendelLocationScores
+"""
+Supply a penetrance for individual i.
+"""
+function penetrance_location_score(person::Person, locus::Locus,
+  multi_genotype::Matrix{Int}, par::Vector{Float64},
+  keyword::Dict{AbstractString, Any}, start::Int, finish::Int, i::Int)
 
+  pen = 1.0
+  for l = start:finish
+    allele1 = multi_genotype[1, l]
+    allele2 = multi_genotype[2, l]
+    loc = locus.model_locus[l]
+    p = 1.0 # for reduced penetrance let p depend on loc
+    pen = p * pen
+  end
+  return pen
+end # function penetrance_location_score
+
+"""
+Supply a prior probability for founder i.
+"""
+function prior_location_score(person::Person, locus::Locus,
+  multi_genotype::Matrix{Int}, par::Vector{Float64},
+  keyword::Dict{AbstractString, Any}, start::Int, finish::Int, i::Int)
+
+  prior_prob = 1.0
+  for l = start:finish
+    loc = locus.model_locus[l]
+    allele = multi_genotype[1, l]
+    frequency = dot(vec(person.admixture[i, :]),
+                    vec(locus.frequency[loc][:, allele]))
+    prior_prob = prior_prob * frequency
+    if !locus.xlinked[loc] || !person.male[i]
+      allele = multi_genotype[2, l]
+      frequency = dot(vec(person.admixture[i, :]),
+                      vec(locus.frequency[loc][:, allele]))
+      prior_prob = prior_prob * frequency
+    end
+  end
+  return prior_prob
+end # function prior_location_score
+
+"""
+Supply the transmission probability that a parent i with a particular
+genotype transmits a particular gamete to his or her child j.
+"""
+function transmission_location_score(person::Person, locus::Locus,
+  gamete::Vector{Int}, multi_genotype::Matrix{Int}, par::Vector{Float64},
+  keyword::Dict{AbstractString, Any}, start::Int, finish::Int, i::Int, j::Int)
+  #
+  # For male to male inheritance at an x-linked locus,
+  # set the transmission probability equal to 1.
+  #
+  loc = locus.model_locus[start]
+  xlinked = locus.xlinked[loc]
+  if xlinked && person.male[i] && person.male[j]
+    return 1.0
+  end
+  #
+  # Find the map location of the trait locus.
+  #
+  if keyword["analysis_option"] == "LocationScores"
+    trait_place = locus.model_locus[locus.trait]
+    locus.morgans[1, trait_place] = par[1]
+    locus.morgans[2, trait_place] = par[end]
+    if locus.trait > 1
+      left = locus.model_locus[locus.trait - 1]
+      d = locus.morgans[1, trait_place] - locus.morgans[1, left]
+      locus.theta[1, locus.trait - 1] = map_function(d, "Haldane")
+      d = locus.morgans[2, trait_place] - locus.morgans[2, left]
+      locus.theta[2, locus.trait - 1] = map_function(d, "Haldane")
+    end
+    if locus.trait < locus.model_loci
+      right = locus.model_locus[locus.trait + 1]
+      d = locus.morgans[1, right] - locus.morgans[1, trait_place]
+      locus.theta[1, locus.trait] = map_function(d, "Haldane")
+      d = locus.morgans[2, right] - locus.morgans[2, trait_place]
+      locus.theta[2, locus.trait] = map_function(d, "Haldane")
+    end
+  end
+  #
+  # Store an indicator of the sex of the parent.
+  #
+  if person.male[i]
+    i_sex = 2
+  else
+    i_sex = 1
+  end
+  #
+  # Reduce the computations by considering only the heterozygous loci.
+  # Use Trow's formula to express the recombination fraction
+  # between two heterozygous loci in terms of the recombination
+  # fractions between the adjacent loci that separate them.
+  # Set the logical variable found to true when the first heterozygous
+  # parental locus is found. Phase records the phase of the most
+  # recent heterozygous parental locus.
+  #
+  trans = 1.0
+  found = false
+  phase = true
+  r = 0.5
+  for l = start:finish
+    match1 = multi_genotype[1, l] == gamete[l]
+    match2 = multi_genotype[2, l] == gamete[l]
+    #
+    # Check whether either the first or second parental gene at
+    # the current locus matches the gamete gene at this locus.
+    # If not, then return with 0 for the transmission probability.
+    #
+    if !match1 && !match2
+      return 0.0
+    end
+    #
+    # Check whether the current locus is heterozygous.
+    #
+    if match1 != match2
+      if found
+        if phase == match1
+          trans = trans * (0.5 + r)
+        else
+          trans = trans * (0.5 - r)
+        end
+      else
+        found = true
+        if start == 1 || start == finish
+          trans = 0.5
+        else
+          trans = 1.0
+        end
+      end
+      phase = match1
+      r = 0.5
+    end
+    if found && l < finish
+      r = r * (1.0 - 2.0 * locus.theta[i_sex, l])
+    end
+  end
+  if !found; trans = 1.0; end
+  return trans
+end # function transmission_location_score
+
+"""
+Initialize the optimization problem.
+"""
+function initialize_optimization_location_score!(locus::Locus,
+  parameter::Parameter, keyword::Dict{AbstractString, Any})
+  #
+  # Initialize, bound, and name the parameters.
+  #
+  if parameter.parameters == 1
+    parameter.name[1] = "theta"
+  else
+    parameter.name[1] = "xxtheta"
+    parameter.name[2] = "xytheta"
+  end
+  small = 1e-4
+  if locus.trait > 1
+    left = locus.model_locus[locus.trait - 1]
+    parameter.min[1] = locus.morgans[1, left] + small
+    parameter.min[end] = locus.morgans[end, left] + small
+  end
+  if locus.trait < locus.model_loci
+    right = locus.model_locus[locus.trait + 1]
+    parameter.max[1] = locus.morgans[1, right] - small
+    parameter.max[end] = locus.morgans[end, right] - small
+  end
+  d = keyword["flanking_distance"]
+  if locus.trait == 1
+    right = locus.model_locus[2]
+    parameter.min[1] = locus.morgans[1, right] - d
+    parameter.min[end] = locus.morgans[end, right] - d
+  elseif locus.trait == locus.model_loci
+    left = locus.model_locus[locus.trait - 1]
+    parameter.max[1] = locus.morgans[1, left] + d
+    parameter.max[end] = locus.morgans[end, left] + d
+  end
+  if keyword["travel"] == "search"
+    parameter.par = 0.5*(parameter.min + parameter.max)
+    if keyword["gender_neutral"]
+      a = (parameter.max[2] - parameter.min[2])
+      b = (parameter.max[1] - parameter.min[1])
+      c = a / b
+      d = parameter.min[2] - c * parameter.min[1]
+      parameter.constraint[1, 1] = c
+      parameter.constraint[1, 2] = -1.0
+      parameter.constraint_level[1] = c * parameter.min[1] - parameter.min[2]
+    end
+  else
+    if parameter.points == 1
+      parameter.grid[1, :] = 0.5*(parameter.min + parameter.max)
+    else
+      for j = 1:parameter.points
+        a = (j - 1.0) / (parameter.points - 1.0)
+        parameter.grid[j, :] = a * parameter.max + (1.0 - a) * parameter.min
+      end
+      parameter.min[1:end] = -Inf
+      parameter.max[1:end] = Inf
+    end
+  end
+  return parameter
+end # function initialize_optimization_location_score!
+
+end # module MendelLocationScores
